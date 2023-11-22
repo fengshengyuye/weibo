512846dc567b4a4a0f281037a842772a3fc5ab65
fengshengyuye <2669328295@qq.com>
2023-11-22 23:34:33 +0800

    用户注册页面

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,4 @@ Route::get('/', function () {
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
+Route::get('/signup','UserController@create')->name('signup');
