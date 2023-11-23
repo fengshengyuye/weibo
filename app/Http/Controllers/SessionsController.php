b04d3b6528794d384c04ac6f33d67e4bea835246
fengshengyuye <2669328295@qq.com>
2023-11-23 23:53:39 +0800

    创建会话

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+use Auth;
+use Illuminate\Http\Request;
+
+class SessionsController extends Controller
+{
+    public function create()
+    {
+        return view('sessions.create');
+    }
+    public function store(Request $request)
+    {
+        $credentials = $this->validate($request, [
+            'email' => 'required|email|max:255',
+            'password' => 'required'
+        ]);
+        if (Auth::attempt($credentials)) {
+            // 登录成功后的相关操作
+            session()->flash('success', '欢迎回来！');
+            return redirect()->route('users.show', [Auth::user()]);
+        } else {
+            // 登录失败后的相关操作
+            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+            return redirect()->back()->withInput();
+        }
+        return;
+    }
+}
