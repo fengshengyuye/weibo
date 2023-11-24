60d8da2385cc7dae755dbad9fbb46222b7fb9bd2
fengshengyuye <2669328295@qq.com>
2023-11-25 00:29:05 +0800

    微博数据模型

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -25,7 +25,10 @@ class UsersController extends Controller
 
     public function show(User $user)
     {
-        return view('users.show', compact('user'));
+        $statuses = $user->statuses()
+            ->orderBy('created_at', 'desc')
+            ->paginate(10);
+        return view('users.show', compact('user', 'statuses'));
     }
 
     public function store(Request $request)
@@ -104,8 +107,8 @@ class UsersController extends Controller
         session()->flash('success', '恭喜你，激活成功！');
         return redirect()->route('users.show', [$user]);
     }
-    public function statuses()
-    {
-        return $this->hasMany(Status::class);
-    }
+//    public function statuses()
+//    {
+//        return $this->hasMany(Status::class);
+//    }
 }
