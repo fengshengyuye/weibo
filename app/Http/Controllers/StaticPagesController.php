be31c14f6d55fdd37153506927e647da907455e2
fengshengyuye <2669328295@qq.com>
2023-11-25 00:55:26 +0800

    首页微博列表

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -1,14 +1,19 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Auth;
 use Illuminate\Http\Request;
 
 class StaticPagesController extends Controller
 {
     public function home()
     {
-        return view('static_pages/home');
+        $feed_items = [];
+        if (Auth::check()) {
+            $feed_items = Auth::user()->feed()->paginate(30);
+        }
+
+        return view('static_pages/home', compact('feed_items'));
     }
 
     public function help()
