ee241463a677ee60d28ec9620dea5201dee5d4f3
fengshengyuye <2669328295@qq.com>
2023-11-24 20:56:36 +0800

    管理员可删除用户

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -72,6 +72,11 @@ class UsersController extends Controller
     {
         $users = User::paginate(10);
         return view('users.index', compact('users'));
-
+    }
+    public function  destroy(User $user){
+        $this->authorize('destroy', $user);
+        $user->delete();
+        session()->flash('success', '成功删除用户！');
+        return back();
     }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -17,6 +17,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'fengshengyuye';
         $user->email = 'ccc@qq.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
